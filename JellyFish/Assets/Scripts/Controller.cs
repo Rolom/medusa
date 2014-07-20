2655dbe0639888af7b3be3ef3c43705522bf5a07
Rolom <rolando.martinez@gmail.com>
2014-07-20 18:20:59 -0400

    Primer commit del proyecto en la vida

---
+++ JellyFish/Assets/Scripts/Controller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	public GameObject character;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (character);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
