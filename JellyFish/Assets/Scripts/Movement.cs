0e68b061bf2b9dffab10c551dd511253b7f21889
Rolom <rolando.martinez@gmail.com>
2014-07-31 20:21:51 -0400

    Agregamos nuevo control para la medusa, no está pulido pero es un comienzo

--- JellyFish/Assets/Scripts/Movement.cs
+++ JellyFish/Assets/Scripts/Movement.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
+	static float LINEAR_VELOCITY = 20.0f;
+	static float MAXIMUM_ROTATION_ANGLE = 60.0f;	//in degrees.
+
 	Vector2 moveLeft;
 	Vector2 moveRight;
 	
@@ -14,23 +17,94 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey (KeyCode.A)) {
-						//print ("a key is held down");
-						rigidbody2D.AddForce (moveLeft);
-						transform.Rotate (Vector3.forward);
-				} else if (Input.GetKey (KeyCode.D)) {
-						//print ("k key is held down");
-						rigidbody2D.AddForce (moveRight);
-						transform.Rotate (Vector3.back);
-				} else {
-						rigidbody2D.AddForce (new Vector2 (0, 0));
-				}
-
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			print ("mouse key is held down");
+			AnimateCharacter();
+		} else {
+			//TODO agregar equilibrio de medusa.
+		}
 	}
 
+	void FixedUpdate(){
+		float range = 1.0f/3.0f;
+		float currentRotation = transform.rotation.z;
 
-	void OnTriggerEnter2D(Collider2D collider) {
-		Debug.Log("HOli");
-		Destroy (gameObject);
+		//print (" preangular: " + transform.rotation.z);
+		print("Angular position: " + currentRotation);
+
+		if( currentRotation < -range || currentRotation > range){
+			rigidbody2D.angularVelocity = 0.0f;
+		}
 	}
+
+	void AnimateCharacter() {
+		Vector2 mouseVector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 characterVector = transform.position;
+		Vector2 newDirection = characterVector - mouseVector;
+		print (characterVector);
+		print (mouseVector);
+		print (newDirection);
+
+		//rigidbody2D.AddForce (newDirection);
+
+		rigidbody2D.AddForce( moveLinearMedusa(newDirection) );
+
+		rigidbody2D.AddTorque( addTorqueForMedusa(newDirection) );
+		//rigidbody2D.AddTorque (0.75f);
+
+		//print ("Local Rotation " + (transform.localRotation.z) * (180/Mathf.PI));
+	}
+
+	float getAngle( Transform transform)
+	{
+		float localAngle = transform.localRotation.z * ( 180/Mathf.PI);
+
+		localAngle = 90 + localAngle;
+		print ("Local angle: " + (localAngle) );
+		return localAngle;
+	}
+
+	Vector2 moveLinearMedusa (Vector2 newDirection)
+	{
+		float vectorMagnitud = inverseVectorMagnitude(newDirection.magnitude);
+
+		Vector2 directionVector = new Vector2();
+
+		print ("Vector Magnitude: " + vectorMagnitud);
+
+		if (newDirection.x < 0) {
+			directionVector.Set(-1 * vectorMagnitud, 0);
+		} else if (newDirection.x > 0) {
+			directionVector.Set(vectorMagnitud, 0);
+		} else {
+			directionVector.Set(0,0);
+		}
+
+		return directionVector;
+	}
+
+	float inverseVectorMagnitude( float magnitude ){
+		return 1 / Mathf.Pow (magnitude, 2) * LINEAR_VELOCITY;
+	}
+
+	float addTorqueForMedusa( Vector2 newDirection ){
+
+		float torque = 1.0f * inverseVectorMagnitude(newDirection.magnitude);
+
+		if (newDirection.x < 0) {
+			// do nothing
+
+		} else if (newDirection.x > 0) {
+			//directionVector.Set(vectorMagnitud, 0);
+			torque *= -1;
+
+		} else {
+			//directionVector.Set(0,0);
+			torque *= 0;
+		}
+
+		print ("magnitud torque: " + torque);
+		return torque;
+	}
+
 }
