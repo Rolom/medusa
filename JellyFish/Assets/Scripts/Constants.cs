2825fb01fccdd8890255d10a89fa08f6e90a48a4
Mauricio Castro <mauricio.castro@mail.udp.cl>
2014-08-11 20:40:23 -0400

    Añadido soport de scenarios dinamicos

---
+++ JellyFish/Assets/Scripts/Constants.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Constants {
+
+	public static string STAGE_TAG = "Stage";
+	public static string JELLYFISH = "Jellyfish";
+}
---
+++ JellyFish/Assets/Scripts/ScenarioScripts/CreateScenarioLimit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateScenarioLimit : MonoBehaviour {
+
+	public StageGameManager stageGameManager;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		
+		if(other.gameObject.tag.Equals(Constants.STAGE_TAG))
+		{
+			stageGameManager.setCanCreateScenario(true);
+		}
+	}
+}
