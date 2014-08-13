b987e04ab27ef6d07fba9a75f0fe3578368d6946
gdeioannes <Trendingames@Trendingamess-MacBook-Pro.local>
2014-08-12 23:37:02 -0400

    etapa funcionando, tentáculos, nuevos srpites

---
+++ JellyFish/Assets/Scripts/MedusaTentacles.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MedusaTentacles : MonoBehaviour{
+	private Color c1 = Color.magenta;
+	private Color c2 = Color.white;
+	private bool activeFlag=false;
+	private LineRenderer lineRenderer;
+
+	private List<Transform> blockList= new List<Transform>();
+	private float distance=1000;
+	private int count=0;
+	private Vector3 distort;
+
+	
+	void Start() {
+		lineRenderer = gameObject.AddComponent<LineRenderer>();
+		lineRenderer.SetWidth(0.1F, 0.0F);
+		lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+		c1.a=0.3f;
+		c2.a=0.1f;
+		lineRenderer.SetColors(c1, c2);
+		foreach(Transform child in this.transform){
+			blockList.Add(child);
+		}
+		lineRenderer.SetVertexCount(blockList.Count);
+	}
+
+	void Update() {
+		if(blockList.Count!=0){
+			count++;
+			lineRenderer=GetComponent<LineRenderer>();
+			for(int i=0;i<blockList.Count;i++){
+				lineRenderer.SetPosition(i, blockList[i].position);
+			}
+
+			GetComponent<LineRenderer>().enabled=true;
+		}else{
+			GetComponent<LineRenderer>().enabled=false;
+			Debug.Log("List Empty");
+		}
+	}
+}
