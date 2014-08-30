31c9408c0f1636a60737d774ee8b93ac39093845
Micho Castro <mauricio.castro@mail.udp.cl>
2014-08-30 16:15:13 -0400

    Added Sound SFX Manager and interface to instance sounds (sounds not implemented yet)

---
+++ JellyFish/Assets/Scripts/SouncScripts/SFXObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface SFXObject {
+
+	void play();
+
+	void play(bool newInstance);
+
+	void stop();
+
+	void pause();
+	
+}
---
+++ JellyFish/Assets/Scripts/SouncScripts/SoundManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+	private static SoundManager _instance;
+
+	public static SoundManager getInstance()
+	{
+		if(_instance == null)
+		{
+			_instance = GameObject.FindObjectOfType<SoundManager>();
+		}
+		return _instance;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
