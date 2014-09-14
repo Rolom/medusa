993b4ad52e7c31f9787c224669ea9ff8f6dc2426
gdeioannes <Trendingames@Trendingamess-MacBook-Pro.local>
2014-09-14 18:48:06 -0300

    Corrections and other.

--- JellyFish/Assets/Scripts/GUI Scripts/OnPlay.cs
+++ JellyFish/Assets/Scripts/GUI Scripts/OnPlay.cs
@@ -5,8 +5,14 @@ public class OnPlay : BasicGUI {
 	
 	private string myScore="0";
 	private GUI score;
+	private string bonus="";
+	private int bonusCount=0;
+	private int saveScore=0;
+	private bool bonusFlag=false;
+	private int intMyScore=0;
 
 	void OnGUI () {
+		intMyScore=ScoreManager.getInstance().getScore();
 		GUI.color=changeAlphaGui(GUI.color);
 
 		GUI.skin=myGuiSkin;
@@ -19,8 +25,25 @@ public class OnPlay : BasicGUI {
 			GUIManager.getInstance().showPause();
 		}
 
+		print (intMyScore-saveScore);
+		if(intMyScore-saveScore>1){
+			bonus="Bonus +"+(intMyScore-saveScore);
+			bonusFlag=true;
+		}
+		saveScore=intMyScore;
+		if(bonusFlag){
+			bonusCount++;
+			if(bonusCount>400){
+				bonusCount=0;
+				bonusFlag=false;
+				bonus="";
+			}
+		}
+
 		GUI.Box(RectAligment.leftRect(1,30,10),Constants.SCORE_TEXT+myScore);
 
+		myGuiSkin.box.fontSize=ProportionFontSize.PorcentageFontSize(8);
+		GUI.Box(RectAligment.leftRect(8,30,10),bonus);
 
 		//hSliderValue = GUI.HorizontalSlider (RectAligment.centerRect(95, 50, 30), hSliderValue, 0f, 1.0f);
 
